95d7db82c7d43b61443144464a21deec05d2f458
Lutz Reinhardt <lutz.reinhardt@elektrobit.com>
2026-06-03 13:08:37 +0000

    feat: Linux namespace for linux-sandbox action
    Some tests require that linux-sandbox works. If not they are either unstable or just do not work. This centralizes the action code in this repo.

---
+++ unblock_user_namespace_for_linux_sandbox/action_callable.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# *******************************************************************************
+# Copyright (c) 2026 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-License-Identifier: Apache-2.0
+# *******************************************************************************
+INSTALL_BASE=$(bazel info install_base)
+sudo bash -c "cat >/etc/apparmor.d/score-linux-sandbox" <<-EOF
+abi <abi/4.0>,
+include <tunables/global>
+
+profile linux-sandbox ${INSTALL_BASE}/linux-sandbox flags=(unconfined) {
+  userns,
+
+  # Site-specific additions and overrides. See local/README for details.
+  include if exists <local/score-linux-sandbox>
+}
+EOF
+sudo apparmor_parser -r /etc/apparmor.d/score-linux-sandbox
+
+${INSTALL_BASE}/linux-sandbox "/bin/true"
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "Warning: '${INSTALL_BASE}/linux-sandbox \"/bin/true\"' failed."
+else
+  echo "Success: '${INSTALL_BASE}/linux-sandbox \"/bin/true\"' succeeded."
+fi
+exit $EXIT_CODE
