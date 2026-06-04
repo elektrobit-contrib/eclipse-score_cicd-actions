30ed908edcf367bc38ebff5b386a244f9b6417a7
lurtz <727209+lurtz@users.noreply.github.com>
2026-06-04 16:30:14 +0200

    feat: Linux user namespace for linux-sandbox action (#5)
    * Some tests require that linux-sandbox works. If not they are either unstable or just do not work. This centralizes the action code in this repo.
    * Also adds devcontainer: It provides a uniform environment across all S-CORE repos and removes the need to setup one on the host.
    
    Signed-off-by: lurtz <727209+lurtz@users.noreply.github.com>
    Co-authored-by: Alexander Lanin <Alexander.Lanin@etas.com>

---
+++ unblock-user-namespace-for-linux-sandbox/action_callable.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# *******************************************************************************
+# Copyright (c) 2026 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-License-Identifier: Apache-2.0
+# *******************************************************************************
+set -euo pipefail
+
+if ! command -v bazel >/dev/null 2>&1; then
+  echo "ERROR: bazel is required for this action"
+  exit 1
+fi
+
+INSTALL_BASE=$(bazel info install_base)
+sudo bash -c "cat >/etc/apparmor.d/score-linux-sandbox" <<-EOF
+abi <abi/4.0>,
+include <tunables/global>
+
+profile linux-sandbox ${INSTALL_BASE}/linux-sandbox flags=(unconfined) {
+  userns,
+
+  # Site-specific additions and overrides. See local/README for details.
+  include if exists <local/score-linux-sandbox>
+}
+EOF
+sudo apparmor_parser -r /etc/apparmor.d/score-linux-sandbox
+
+set +e
+"${INSTALL_BASE}/linux-sandbox" "/bin/true"
+EXIT_CODE=$?
+set -e
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "Error: '${INSTALL_BASE}/linux-sandbox /bin/true' failed."
+else
+  echo "Success: '${INSTALL_BASE}/linux-sandbox /bin/true' succeeded."
+fi
+exit $EXIT_CODE
